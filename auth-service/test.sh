75fdaf30c3fd7e1a642f513def5b24222e801908
warren vosper <straydogsw@gmail.com>
2025-08-24 12:33:38 -0500

    parameterize test.sh

--- auth-service/test.sh
+++ auth-service/test.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -x
 
-curl -v -d '{}' -H 'Content-Type: application/json' localhost:3000/signup
+host=${1:-localhost}
 
-curl -v -d '{"email": "user@example.com"}' -H 'Content-Type: application/json' localhost:3000/signup
+curl -v -d '{}' -H 'Content-Type: application/json' $host:3000/signup
 
-curl -v -d '{"email": "user@example.com", "password": "password123"}' -H 'Content-Type: application/json' localhost:3000/signup
+curl -v -d '{"email": "user@example.com"}' -H 'Content-Type: application/json' $host:3000/signup
 
-curl -v -d '{"email": "user@example.com", "password": "password123", "requires2FA": false}' -H 'Content-Type: application/json' localhost:3000/signup
+curl -v -d '{"email": "user@example.com", "password": "password123"}' -H 'Content-Type: application/json' $host:3000/signup
+
+curl -v -d '{"email": "user@example.com", "password": "password123", "requires2FA": false}' -H 'Content-Type: application/json' $host:3000/signup
