cfdee7d2eb588b0513bea937f1baed87d7485bae
warren vosper <straydogsw@gmail.com>
2025-08-24 12:28:15 -0500

    add sign-up route interface

---
+++ auth-service/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -x
+
+curl -v -d '{}' -H 'Content-Type: application/json' localhost:3000/signup
+
+curl -v -d '{"email": "user@example.com"}' -H 'Content-Type: application/json' localhost:3000/signup
+
+curl -v -d '{"email": "user@example.com", "password": "password123"}' -H 'Content-Type: application/json' localhost:3000/signup
+
+curl -v -d '{"email": "user@example.com", "password": "password123", "requires2FA": false}' -H 'Content-Type: application/json' localhost:3000/signup
--- auth-service/tests/api/helpers.rs
+++ auth-service/tests/api/helpers.rs
@@ -1,4 +1,5 @@
 use auth_service::Application;
+use uuid::Uuid;
 
 pub struct TestApp {
     pub address: String,
@@ -34,9 +35,20 @@ impl TestApp {
             .expect("Failed to execute request.")
     }
 
-    pub async fn post_signup(&self) -> reqwest::Response {
+    //pub async fn post_signup(&self) -> reqwest::Response {
+    //    self.http_client
+    //        .post(&format!("{}/signup", &self.address))
+    //        .send()
+    //        .await
+    //        .expect("Failed to execute request.")
+    //}
+    pub async fn post_signup<Body>(&self, body: &Body) -> reqwest::Response
+    where
+        Body: serde::Serialize,
+    {
         self.http_client
             .post(&format!("{}/signup", &self.address))
+            .json(body)
             .send()
             .await
             .expect("Failed to execute request.")
@@ -73,4 +85,8 @@ impl TestApp {
             .await
             .expect("Failed to execute request.")
     }
+}
+
+pub fn get_random_email() -> String {
+    format!("{}@example.com", Uuid::new_v4())
 }
